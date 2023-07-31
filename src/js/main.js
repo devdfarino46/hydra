8ffe3b26f44520e36f4b8eb453f033857d036ad2
devdfarino46 <dev.dfarino46@gmail.com>
2023-07-31 22:38:17 +0300

    Sections: header, footer, menu, hero, techno - done.

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,18 @@
 let menuBtn = document.querySelector('.menu__btn');
 let menu = document.querySelector('.menu');
+let menuWrapper = document.querySelector('.menu__wrapper');
 let menuClose = document.querySelector('.menu__close');
 
 if (menuBtn && menu) {
     menuBtn.addEventListener('click', ev => {
         menu.classList.add('_active');
+        menuWrapper.classList.add('_active');
         document.body.classList.add('_no-scroll');
     })
 
     menuClose.addEventListener('click', ev => {
         menu.classList.remove('_active');
+        menuWrapper.classList.remove('_active');
         document.body.classList.remove('_no-scroll');
     })
 }
